a437ba69c88a5fe0346e09138f540157d071f356
m <mcallara@gmail.com>
2024-01-10 23:34:10 +0100

    fix(sr): rs

--- example/sample.py
+++ example/sample.py
@@ -1,3 +1,5 @@
 # content of sample.py some change another
+# https://mestrak.com/blog/semantic-release-with-python-poetry-github-actions-20nn
+# https://python-semantic-release.readthedocs.io/en/latest/configuration.html
 def func(x):
     return x + 1
